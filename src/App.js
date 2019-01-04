5aae7303f6ce40627a08c295b496679aa85565e0
Matthew Eldredge <mattheweldredge@gmail.com>
2019-01-04 14:35:03 -0800

    refactor App as simple functional component that displays like index

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Chat from './containers/Chat'
 import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header"></header>
-        <main>
-          <Chat/>
-        </main>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <main>
+        <Chat/>
+      </main>
+    </div>
+  );
 }
 
-export default App;
