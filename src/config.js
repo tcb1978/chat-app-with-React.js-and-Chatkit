2a7f45391d26f0bc79fdad6f9aa9cd07927fafb6
Matthew Eldredge <mattheweldredge@gmail.com>
2019-01-04 14:19:43 -0800

    test message list with dummy data, wired up chatkit in app, and initialized state, return latest message in state

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/b499d079-8ad3-40d3-9be2-3fa485e3b4de/token";
+const instanceLocator = "v1:us1:b499d079-8ad3-40d3-9be2-3fa485e3b4de";
+
+export { tokenUrl, instanceLocator }
